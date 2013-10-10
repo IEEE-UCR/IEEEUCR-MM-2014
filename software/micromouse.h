346fc046bcbb58bb1c41c0913d20d8b953b8fe70
brandon <brandon@Gravygod>
2013-10-10 04:09:49 -0700

    Generated STL files (most are not critical) Updated micromouse code a bit (far from done)

--- software/micromouse.h
+++ software/micromouse.h
@@ -20,12 +20,13 @@
  * Let's lay down a few ground rules to make our micromice run well.
  * The first thing is that our turn directions should follow this format...
  **/
-enum turn_direction {left = 0, right = 1};
+enum eTurnDirection {left = 0, right = 1};
 
 /*
  * Another thing that should be set forth is the direction that our mice would face...
  **/
-enum face_direction {north = 0, east = 1, south = 2, west = 3};
+enum eFaceDirection {north = 0, east = 1, south = 2, west = 3,
+    northEast = 4, southEast = 5, southWest = 6, northWest = 7};
 
 /*
  * What basically will go on from now is the declaration of our mouse IDs 
@@ -43,5 +44,32 @@ enum face_direction {north = 0, east = 1, south = 2, west = 3};
  **/
 #define MOUSE_ID 0x0000
 #endif 
+
+/*
+ * Here are some of the actual nylon pinouts...
+ * Bear in mind that these are not intended for STM32 yet. (STM32 should come a bit later...)
+ **/
+
+#if MOUSE_ID = 0x0000
+
+#define generation 0
+#define layout 0
+
+#define UARTtx 0
+#define UARTrx 1
+
+#define lMotorDir 7
+#define rMotorDir 8
+#define lMotorEn 9
+#define rMotorEn 10
+
+#define sensorPinL "A0"
+#define sensorPinC "A1"
+#define sensorPinR "A2"
+
+#define lMotorEnc 11
+#define rMotorEnc 12
+
+#endif // from MOUSE_ID = 0x0000
 
 #endif // from MICROMOUSE_HEADER
